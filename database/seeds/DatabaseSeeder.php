4ce58c1853edcb83b96b7db3fb5bef3319df37e8
Anisur Rahman <anisurlist3@gmail.com>
2020-01-13 18:33:54 +0600

    Seed Completed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\User;
+use App\Product;
+use App\Category;
+use App\Transaction;
+use App\CategoryProduct;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +17,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+    	DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+    	User::truncate();
+    	Category::truncate();
+    	Product::truncate();
+    	Transaction::truncate();
+    	CategoryProduct::truncate();
+
+
+    	factory(User::class, 200)->create();
+    	factory(Category::class, 30)->create();
+    	factory(Product::class, 1000)->create()->each(function($product) {
+    		$categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
+    		$product->categories()->attach($categories);
+    	});
+    	factory(Transaction::class, 1000)->create();
+
+
         // $this->call(UsersTableSeeder::class);
     }
 }
