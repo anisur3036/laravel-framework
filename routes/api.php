24cddee2f024e46d0974faa07e848e470f420e9d
Anisur Rahman <anisurlist3@gmail.com>
2020-01-13 18:36:05 +0600

    define api route and adding images

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,26 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
+//buyers
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+// category
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+//product
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+//Seller
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+//transictions
+Route::resource('transictions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+//user
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
+
+
+// Route::group(['middleware' => 'cors'], function() {
+//    Route::post('/user','UserInfoController@store' );
 // });
-
-// Route::post('user', 'UserInfoController@store');
-
-Route::group(['middleware' => 'cors'], function() {
-   Route::post('/user','UserInfoController@store' );
-});
